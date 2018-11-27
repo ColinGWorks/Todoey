2000320e21e127d087a344f6dd623c7151f7264e
ColinG <colingworks@gmail.com>
2018-11-27 07:43:41 -0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Colin Gui on 11/27/18.
+//  Copyright © 2018 ColinGuiWorks. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
